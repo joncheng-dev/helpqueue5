38258baefb274c70825a267f13a25c175962fcd9
Jonathan Cheng <jcheng461@gmail.com>
2022-10-13 23:30:44 +0900

    new ticket form collects user input and stores in an object -- saved in state. TicketList displays all existing tickets

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,29 +1,11 @@
 import React from "react";
 import Ticket from "./Ticket";
 
-function TicketList() {
-  const ticketList = [
-    {
-      names: "Bart and Lisa",
-      location: "Springfield Elementary",
-      issue: "Martin's stuck on swing",
-    },
-    {
-      names: "Barney and Homer",
-      location: "Moe's Tavern",
-      issue: "Moe's out of beer",
-    },
-    {
-      names: "Willy and Skinner",
-      location: "Springfield Elementary",
-      issue: "Cat stuck in tree",
-    },
-  ];
-
+function TicketList(props) {
   return (
     <React.Fragment>
       <hr />
-      {ticketList.map((ticket) => (
+      {props.ticketList.map((ticket) => (
         <Ticket names={ticket.names} location={ticket.location} issue={ticket.issue} />
       ))}
     </React.Fragment>
